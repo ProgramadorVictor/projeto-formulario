47676280fdb995c44d2cef8ca5b0952df8c87613
ProgramadorVictor <victorochaandrade@gmail.com>
2025-04-24 21:13:01 -0300

    adicionando a lib 'maska' e corrigindo a tag 'spam'

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Maska from 'maska'
 
-createApp(App).mount('#app')
+const app = createApp(App) //Criamos uma váriavel para configurar a aplicação
+app.use(Maska) //Utilizamos o plugin que queremos e adicionamos antes de monta a aplicação. npm install maska@v1.5.0
+app.mount('#app') //Depois, montamos a aplicação
