65702e2a933dfb8dc4e451c664d22eda313a8b93
ProgramadorVictor <victorochaandrade@gmail.com>
2025-04-25 23:06:43 -0300

    adicionando mascara aos campos cep,cpf,cnpj e cartão de credito

--- src/components/FormularioComponent.vue
+++ src/components/FormularioComponent.vue
@@ -107,6 +107,35 @@
                         </div>
                     </div>
                     <div class="mb-3 row">
+                        <label class="col-3 col-form-label">CEP:</label>
+                        <div class="col">
+                            <input type="text" class="form-control" v-maska="'#####-###'" v-model="form.cep">
+                            <small class="text-muted">Formato: 00000-000</small>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">CPF:</label>
+                        <div class="col">
+                            <input type="text" class="form-control" v-maska="'###.###.###-##'" v-model="form.cpf">
+                            <small class="text-muted">Formato: 000.000.000-00</small>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">CNPJ:</label>
+                        <div class="col">
+                            <input type="text" class="form-control" v-maska="'##.###.###/####-##'" v-model="form.cnpj">
+                            <small class="text-muted">Formato: 00.000.000/0000-00</small>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">Cartão de Credito:</label>
+                        <div class="col">
+                            <input type="text" class="form-control" v-maska="'#### #### #### ####'" v-model="form.cartaoDeCredito">
+                            <small class="text-muted">Formato: 0000 0000 0000 0000</small>
+                        </div>
+                    </div>
+
+                    <div class="mb-3 row">
                         <label class="col-3 col-form-label">Data:</label>
                         <div class="col">
                             <input type="date" class="form-control">
@@ -195,7 +224,7 @@
                     <span>Idade: {{ form.idade }}</span>
                 </div>
                 <div class="mb-3 row">
-                    <span>Gênero:</span>
+                    <span>Gênero: {{ form.genero }}</span>
                 </div>
                 <div class="mb-3 row">
                     <span>Licença: {{ form.licenca }}</span>
@@ -208,6 +237,18 @@
                 </div>
                 <div class="mb-3 row">
                     <span>Telefone: {{ form.telefone }}</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>CEP: {{ form.cep }}</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>CPF: {{ form.cpf }}</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>CNPJ: {{ form.cnpj }}</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>Cartão de Credito: {{ form.cartaoDeCredito }}</span>
                 </div>
                 <div class="mb-3 row">
                     <span>Data:</span>
@@ -256,6 +297,10 @@ export default {
             interesses: [],
             genero: '',
             telefone: '',
+            cep: '',
+            cpf: '',
+            cnpj: '',
+            cartaoDeCredito: '',
         }
     })
 }
